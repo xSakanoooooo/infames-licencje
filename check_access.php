bed0354ad49c2432d281125f926d6fb075d35cc2
xSakanoooooo <aaabbb9999po@gmail.com>
2024-10-27 00:35:46 +0200

    Update check_access.php

--- check_access.php
+++ check_access.php
@@ -5,8 +5,29 @@ if (!in_array($_SERVER['REMOTE_ADDR'], $allowed_ips)) {
     header('HTTP/1.0 403 Forbidden');
     exit('Access denied');
 }
-
-// Jeśli IP jest dozwolone, przekieruj do pliku HTML
-header('Location: index.html');
-exit();
 ?>
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pełnoekranowy iframe</title>
+    <style>
+        html, body {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            overflow: hidden;
+        }
+        iframe {
+            width: 100%;
+            height: 100%;
+            border: none; 
+        }
+    </style>
+</head>
+<body>
+    <iframe src="https://script.google.com/macros/s/AKfycbwm7N-UdxaRufswaF0ttoKVnyCf8Pvcd5KW5J2ozaPgymxzJtAiDsUrxiBkjJl47hkqtg/exec"></iframe>
+</body>
+</html>
